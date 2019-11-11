d3bdb5a0c549498004d6c2542d2ca39fba6c1447
priyanka bharti <priyanka.bharti@unthinkable.co>
2019-11-11 23:18:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+ class App extends Component {
+
+  constructor(props){
+    super(props)
+  }
+  render()
+  {
+    return (
+    <div className="App">hi
+
+    </div>
+    );
+  }
+}
+
+export default App;
